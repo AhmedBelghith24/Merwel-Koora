4bd8e39ce6feccb0fb1ba1031f5c19ed59971cff
Ahmed Belghith <abelghith@oakland.edu>
2025-06-27 11:30:00 -0400

    Update ProductRating.tsx

--- src/components/single-product/ProductRating.tsx
+++ src/components/single-product/ProductRating.tsx
@@ -1,15 +1,13 @@
 import { FaStar } from 'react-icons/fa'
 
-async function ProductRating({ productId }: { productId: string }) {
+function ProductRating() {
   const rating = 4.2
   const count = 25
 
-  const className = `flex gap-1 items-center text-md mt-1 mb-4`
-  const countValue = `(${count}) reviews`
   return (
-    <span className={className}>
+    <span className="flex gap-1 items-center text-md mt-1 mb-4">
       <FaStar className="w-3 h-3" />
-      {rating} {countValue}
+      {rating} ({count}) reviews
     </span>
   )
 }

4702d19b6aa230a8d804acf05122878d2471155e
Ahmed Belghith <abelghith@oakland.edu>
2025-07-03 01:06:22 +0100

    Update page.tsx 11

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -5,7 +5,16 @@ import { formatCurrency } from '@/utils/format'
 import FavoriteToggleButton from '@/components/products/FavoriteToggleButton'
 import AddToCart from '@/components/single-product/AddToCart'
 import ProductRating from '@/components/single-product/ProductRating'
-async function SingleProductPage({ params }: { params: { id: string } }) {
+
+// Define the correct type for the props
+type Props = {
+  params: {
+    id: string
+  }
+}
+
+// Apply the type to the component props
+async function SingleProductPage({ params }: Props) {
   const product = await fetchSingleProduct(params.id)
   const { name, image, company, description, price } = product
   const dollarsAmount = formatCurrency(price)

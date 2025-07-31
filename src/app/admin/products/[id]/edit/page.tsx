8d51d8c013d692d0e5dab3c5d9e082a512697c3e
Ahmed Belghith <abelghith@oakland.edu>
2025-07-31 14:10:30 -0400

    Update page.tsx

--- src/app/admin/products/[id]/edit/page.tsx
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -1,16 +1,29 @@
-type EditProductPageProps = {
-  params: { id: string }
-}
+import {
+  fetchAdminProductDetails,
+  updateProductAction,
+  updateProductImageAction,
+} from '@/utils/actions'
+import FormContainer from '@/components/form/FormContainer'
+import FormInput from '@/components/form/FormInput'
+import PriceInput from '@/components/form/PriceInput'
+import TextAreaInput from '@/components/form/TextAreaInput'
+import { SubmitButton } from '@/components/form/Buttons'
+import CheckboxInput from '@/components/form/CheckboxInput'
+import ImageInputContainer from '@/components/form/ImageInputContainer'
 
-export default async function EditProductPage({ params }: EditProductPageProps) {
-  const { id } = params
-  const product = await fetchAdminProductDetails(id)
+export default async function EditProductPage({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const product = await fetchAdminProductDetails(params.id)
   const { name, company, description, featured, price } = product
 
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">update product</h1>
 
+      {/* 🖼️ Separate form for updating image */}
       <div className="border p-8 rounded-md mb-8">
         <ImageInputContainer
           action={updateProductImageAction}
@@ -18,15 +31,16 @@ export default async function EditProductPage({ params }: EditProductPageProps) 
           image={product.image}
           text="update image"
         >
-          <input type="hidden" name="id" value={id} />
+          <input type="hidden" name="id" value={params.id} />
           <input type="hidden" name="url" value={product.image} />
         </ImageInputContainer>
       </div>
 
+      {/* 📝 Form for updating product details */}
       <div className="border p-8 rounded-md">
         <FormContainer action={updateProductAction}>
           <div className="grid gap-4 md:grid-cols-2 my-4">
-            <input type="hidden" name="id" value={id} />
+            <input type="hidden" name="id" value={params.id} />
             <FormInput
               type="text"
               name="name"

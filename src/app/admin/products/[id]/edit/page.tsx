09adb240bc8e37b788bf58b18b2fb71b6c24096a
Ahmed Belghith <abelghith@oakland.edu>
2025-10-13 21:25:55 +0100

    Update page.tsx3

--- src/app/admin/products/[id]/edit/page.tsx
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -1,6 +1,5 @@
-import { fetchProductDetails } from '@/utils/actions'
+import { fetchSingleProduct } from '@/utils/actions'
 import { formatCurrency } from '@/utils/format'
-import { redirect } from 'next/navigation'
 
 type ProductPageProps = {
   params: Promise<{
@@ -12,20 +11,9 @@ export default async function ProductPage({ params }: ProductPageProps) {
   // Await the params Promise
   const { id } = await params;
 
-  let product;
+  const product = await fetchSingleProduct(id);
 
-  try {
-    product = await fetchProductDetails(id);
-  } catch (error) {
-    console.error("Failed to fetch product:", error);
-    redirect('/');
-  }
-
-  if (!product) {
-    redirect('/');
-  }
-
-  const { image, title, price, description } = product;
+  const { image, name, price, description } = product;
 
   return (
     <section className="mt-6">
@@ -33,13 +21,13 @@ export default async function ProductPage({ params }: ProductPageProps) {
         {/* Image */}
         <img
           src={image}
-          alt={title}
+          alt={name}
           className="w-96 h-96 object-cover rounded-lg lg:w-full"
         />
 
         {/* Product Info */}
         <div>
-          <h1 className="text-3xl font-semibold capitalize">{title}</h1>
+          <h1 className="text-3xl font-semibold capitalize">{name}</h1>
           <p className="mt-3 text-xl text-muted-foreground">
             {formatCurrency(price)}
           </p>
